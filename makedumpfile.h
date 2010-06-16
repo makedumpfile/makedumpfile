060be6f12ea92345a5282e731e592ac3344a62c5
Cliff Wickman <cpw@sgi.com>
2010-06-16 08:51:12 -0500

    makedumpfile: works on 2.6.32
    LATEST_VERSION is updated to KERNEL_VERSION(2, 6, 32), as this seems to
    work on 2.6.32 and prevents a makedumpfile grumbling.
    
    Tested on SGI UV, which is x86_64.
    
    Diffed against makedumpfile-1.3.5
    
    Signed-off-by: Cliff Wickman <cpw@sgi.com>
    Signed-off-by: Masayuki Igawa <igawa@mxs.nes.nec.co.jp>

--- makedumpfile.h
+++ makedumpfile.h
@@ -449,7 +449,7 @@ do { \
 #define KVER_MIN_SHIFT 16
 #define KERNEL_VERSION(x,y,z) (((x) << KVER_MAJ_SHIFT) | ((y) << KVER_MIN_SHIFT) | (z))
 #define OLDEST_VERSION		KERNEL_VERSION(2, 6, 15)/* linux-2.6.15 */
-#define LATEST_VERSION		KERNEL_VERSION(2, 6, 31)/* linux-2.6.31 */
+#define LATEST_VERSION		KERNEL_VERSION(2, 6, 32)/* linux-2.6.32 */
 
 /*
  * vmcoreinfo in /proc/vmcore

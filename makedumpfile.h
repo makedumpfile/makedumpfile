69bbfaf922ed85b5fafc02d58404590623b84ae1
Masayuki Igawa <igawa@mxs.nes.nec.co.jp>
2010-06-17 10:17:18 +0900

    [PATCH] Add linux-2.6.32 - 2.6.34 support.
    A new makedumpfile has been tested on linux-2.6.32 - 2.6.34 so it can
    support their kernels.
    
    Thanks
    Masayuki Igawa
    
    Signed-off-by: Masayuki Igawa <igawa@mxs.nes.nec.co.jp>

--- makedumpfile.h
+++ makedumpfile.h
@@ -449,7 +449,7 @@ do { \
 #define KVER_MIN_SHIFT 16
 #define KERNEL_VERSION(x,y,z) (((x) << KVER_MAJ_SHIFT) | ((y) << KVER_MIN_SHIFT) | (z))
 #define OLDEST_VERSION		KERNEL_VERSION(2, 6, 15)/* linux-2.6.15 */
-#define LATEST_VERSION		KERNEL_VERSION(2, 6, 32)/* linux-2.6.32 */
+#define LATEST_VERSION		KERNEL_VERSION(2, 6, 34)/* linux-2.6.34 */
 
 /*
  * vmcoreinfo in /proc/vmcore

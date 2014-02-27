12491a56f2394dc9199926c812d3e0a42b8082a6
Aruna Balakrishnaiah <aruna@linux.vnet.ibm.com>
2014-02-27 15:32:44 +0900

    [PATCH 09/11] eppic: Scrub buffers involved in guest block I/O.
    vhost_scsi instance will be attached to the file's private data.
    To get to the right file check the fdtable for each task, if the file
    has registered its fops with vhost_net_open, if so we can retreive the
    file's private data.
    
    if (task->files->fdt->fd[i]->f_op->open == &vhost_scsi_open)
    	vhost_scsi *vs = task->files->fdt->fd[i]->private_data;
    
    struct vhost_virtqueue *vq = (struct vhost_virtqueue *)vs->vqs[i].vq;
    scrub vq->iov[j].iov_base
    
    Signed-off-by: Aruna Balakrishnaiah <aruna@linux.vnet.ibm.com>

---
+++ eppic_scripts/vhost_scsi_buffers.c
@@ -0,0 +1,71 @@
+string
+vhost_opt()
+{
+	    return "l";
+}
+
+string
+vhost_usage()
+{
+	    return "\n";
+}
+
+static void
+vhost_showusage()
+{
+	    printf("usage : vhost %s", vhost_usage());
+}
+
+string
+vhost_help()
+{
+	    return "Help";
+}
+
+void
+vhost_scsi(struct vhost_scsi *vs)
+{
+	if (vs == NULL)
+		return;
+
+	for (i = 0; i < 128; i++) {
+		struct vhost_virtqueue *vq = (struct vhost_virtqueue *)vs->vqs[i].vq;
+
+		for (j = 0; j < 1024; j++)
+		      memset((char *)vq->iov[j].iov_base, 'L', vq->iov[j].iov_len);
+		      memset((char *)&(vq->iov[j].iov_len), 'L', 0x8);
+	}
+}
+
+int
+vhost()
+{
+	struct list_head *head, *next;
+	struct task_struct *tsk;
+
+	tsk = &init_task;
+
+	head = (struct list_head *) &(tsk->tasks);
+	next = (struct list_head *) tsk->tasks.next;
+
+	while (next != head)
+	{
+		int i;
+		struct task_struct *task, *off = 0;
+
+		task = (struct task_struct *)((unsigned long)next - ((unsigned long)&(off->tasks)));
+
+		if (task->files && task->files->fdt) {
+			for (i = 0; i < task->files->fdt->max_fds; i++) {
+				if (task->files->fdt->fd[i] && task->files->fdt->fd[i]->f_op
+					&& task->files->fdt->fd[i]->f_op->open == &vhost_scsi_open)
+					vhost_scsi((struct vhost_scsi *)task->files->fdt->fd[i]->private_data);
+			}
+		}
+
+
+		next = (struct list_head *)task->tasks.next;
+	}
+
+	return 1;
+}

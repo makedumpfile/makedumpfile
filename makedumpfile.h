deac85e1f42edf0a85e8669fd5c956c1e84e6bcd
Atsushi Kumagai <ats-kumagai@wm.jp.nec.com>
2015-10-28 13:36:20 +0900

    [PATCH] Support newer kernels.
    A new makedumpfile supports newer kernels:
    
          - 4.0, 4.1    (x86 FLATMEM)
          - 4.0, 4.1    (x86 SPARSEMEM)
          - 4.0, 4.1    (x86_64 SPARSEMEM)
    
    Signed-off-by: Atsushi Kumagai <ats-kumagai@wm.jp.nec.com>

--- makedumpfile.h
+++ makedumpfile.h
@@ -456,7 +456,7 @@ do { \
 #define KVER_MIN_SHIFT 16
 #define KERNEL_VERSION(x,y,z) (((x) << KVER_MAJ_SHIFT) | ((y) << KVER_MIN_SHIFT) | (z))
 #define OLDEST_VERSION		KERNEL_VERSION(2, 6, 15)/* linux-2.6.15 */
-#define LATEST_VERSION		KERNEL_VERSION(3, 19, 0)/* linux-3.19.0 */
+#define LATEST_VERSION		KERNEL_VERSION(4, 1, 0)/* linux-4.1.0 */
 
 /*
  * vmcoreinfo in /proc/vmcore

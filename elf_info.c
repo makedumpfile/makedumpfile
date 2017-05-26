makedumpfile/makedumpfile

commit f10d1e2e94c50d6c4ef32785277255866ddf0972

[PATCH] elf_info.c: fix memory leak in get_kcore_dump_loads()
Fix memory leaks for error path in get_kcore_dump_loads().
This is not actual issue but bad manner.

Signed-off-by: Pingfan Liu <piliu@redhat.com>

elf_info.c
@@ -890,12 +890,14 @@ int get_kcore_dump_loads(void)
 				|| !is_phys_addr(p->virt_start))
 			continue;
 		if (j >= loads)
+			free(pls);
 			return FALSE;
 
 		if (j == 0) {
 			offset_pt_load_memory = p->file_offset;
 			if (offset_pt_load_memory == 0) {
 				ERRMSG("Can't get the offset of page data.\n");
+				free(pls);
 				return FALSE;
 			}
 		}

commit a569d632ddbf157bfaeccabda61c2960daf3c7b7

[PATCH] Fix get_kcore_dump_loads() error case
commit f10d1e2e94c50 introduced another bug while fixing memory leak.
Use the braces with if condition.

Signed-off-by: Pratyush Anand <panand@redhat.com>

elf_info.c
@@ -889,9 +889,10 @@ int get_kcore_dump_loads(void)
 		if (p->phys_start == NOT_PADDR
 				|| !is_phys_addr(p->virt_start))
 			continue;
-		if (j >= loads)
+		if (j >= loads) {
 			free(pls);
 			return FALSE;
+		}
 
 		if (j == 0) {
 			offset_pt_load_memory = p->file_offset;
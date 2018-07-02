4fd05a5e33f8876c9d404576ee21a5a29732a68f
Kazuhito Hagio <k-hagio@ab.jp.nec.com>
2018-07-02 14:29:35 -0400

    [PATCH] Support newer kernels.
    A new makedumpfile supports newer kernels:
    
        - 4.15, 4.16, 4.17   (x86 FLATMEM)
        - 4.15, 4.16, 4.17   (x86 SPARSEMEM)
        - 4.15, 4.16, 4.17   (x86_64 SPARSEMEM)
    
    Signed-off-by: Masaki Tachibana <mas-tachibana@vf.jp.nec.com>
    Signed-off-by: Kazuhito Hagio <k-hagio@ab.jp.nec.com>

--- makedumpfile.h
+++ makedumpfile.h
@@ -495,7 +495,7 @@ do { \
 #define KVER_MIN_SHIFT 16
 #define KERNEL_VERSION(x,y,z) (((x) << KVER_MAJ_SHIFT) | ((y) << KVER_MIN_SHIFT) | (z))
 #define OLDEST_VERSION		KERNEL_VERSION(2, 6, 15)/* linux-2.6.15 */
-#define LATEST_VERSION		KERNEL_VERSION(4, 14, 8)/* linux-4.14.8 */
+#define LATEST_VERSION		KERNEL_VERSION(4, 17, 0)/* linux-4.17.0 */
 
 /*
  * vmcoreinfo in /proc/vmcore

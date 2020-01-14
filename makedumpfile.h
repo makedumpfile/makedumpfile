a4fe839d25fba54c5feeae8550a51212ecf8dd5f
Kazuhito Hagio <k-hagio-ab@nec.com>
2020-01-14 15:30:02 -0500

    [PATCH] Support newer kernels up to v5.4
    Support newer kernels as follows:
    
      - 5.2, 5.3, 5.4  (x86_64 SPARSEMEM)
      - 5.4            (x86 FLATMEM, SPARSEMEM)
    
    Signed-off-by: Kazuhito Hagio <k-hagio-ab@nec.com>

--- makedumpfile.h
+++ makedumpfile.h
@@ -495,7 +495,7 @@ do { \
 #define KVER_MIN_SHIFT 16
 #define KERNEL_VERSION(x,y,z) (((x) << KVER_MAJ_SHIFT) | ((y) << KVER_MIN_SHIFT) | (z))
 #define OLDEST_VERSION		KERNEL_VERSION(2, 6, 15) /* linux-2.6.15 */
-#define LATEST_VERSION		KERNEL_VERSION(5, 1, 9) /* linux-5.1.9 */
+#define LATEST_VERSION		KERNEL_VERSION(5, 4, 8) /* linux-5.4.8 */
 
 /*
  * vmcoreinfo in /proc/vmcore

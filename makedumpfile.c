makedumpfile/makedumpfile

commit 2651d5719a211fcebdc3ada489c64bd6fb55ae5e

[PATCH 11/14] fix memory leak in init_xen_crash_info()
Found using -fanalyzer

makedumpfile.c: In function ‘init_xen_crash_info’:
makedumpfile.c:9495:17: warning: leak of ‘buf’ [CWE-401] [-Wanalyzer-malloc-leak]
 9495 |                 return FALSE;
      |                 ^~~~~~

Signed-off-by: Philipp Rudo <prudo@redhat.com>

makedumpfile.c
@@ -9417,6 +9417,7 @@ init_xen_crash_info(void)
 {
 	off_t		offset_xen_crash_info;
 	unsigned long	size_xen_crash_info;
+	int		ret = FALSE;
 	void		*buf;
 
 	get_xen_crash_info(&offset_xen_crash_info, &size_xen_crash_info);
@@ -9441,13 +9442,13 @@ init_xen_crash_info(void)
 	if (lseek(info->fd_memory, offset_xen_crash_info, SEEK_SET) < 0) {
 		ERRMSG("Can't seek the dump memory(%s). %s\n",
 		       info->name_memory, strerror(errno));
-		return FALSE;
+		goto out_error;
 	}
 	if (read(info->fd_memory, buf, size_xen_crash_info)
 	    != size_xen_crash_info) {
 		ERRMSG("Can't read the dump memory(%s). %s\n",
 		       info->name_memory, strerror(errno));
-		return FALSE;
+		goto out_error;
 	}
 
 	info->xen_crash_info.com = buf;
@@ -9458,7 +9459,11 @@ init_xen_crash_info(void)
 	else
 		info->xen_crash_info_v = 0;
 
-	return TRUE;
+	ret = TRUE;
+
+out_error:
+	free(buf);
+	return ret;
 }
 
 int

commit a2136943b1f173d2bf7efffc29542556e38aa564

[PATCH] xen: Fix wrong free issue in init_xen_crash_info()
The commit 2651d5719a21 ("[PATCH 11/14] fix memory leak in
init_xen_crash_info()") changed the behaviour of the function and the
buf variable is always released, but it's still used later when
returning TRUE.  Without the patch, this leads to random data in the
buffer and later to the following error:

  get_xen_basic_info_x86_64: Can't get the symbol of xenheap_phys_end.

Fixes: 2651d5719a21 ("[PATCH 11/14] fix memory leak in init_xen_crash_info()")
Signed-off-by: Dietmar Hahn <dietmar.hahn@fujitsu.com>
Signed-off-by: Kazuhito Hagio <k-hagio-ab@nec.com>

makedumpfile.c
@@ -9668,7 +9668,6 @@ init_xen_crash_info(void)
 {
 	off_t		offset_xen_crash_info;
 	unsigned long	size_xen_crash_info;
-	int		ret = FALSE;
 	void		*buf;
 
 	get_xen_crash_info(&offset_xen_crash_info, &size_xen_crash_info);
@@ -9710,11 +9709,11 @@ init_xen_crash_info(void)
 	else
 		info->xen_crash_info_v = 0;
 
-	ret = TRUE;
+	return TRUE;
 
 out_error:
 	free(buf);
-	return ret;
+	return FALSE;
 }
 
 int
@@ -12377,6 +12376,8 @@ out:
 			free(info->dump_header);
 		if (info->splitting_info != NULL)
 			free(info->splitting_info);
+		if (info->xen_crash_info.com != NULL)
+			free(info->xen_crash_info.com);
 		if (info->p2m_mfn_frame_list != NULL)
 			free(info->p2m_mfn_frame_list);
 		if (info->page_buf != NULL)
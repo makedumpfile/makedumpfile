a5779893ee087409b2d1fe391ead102defe0f00b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2022-11-02 13:00:00 +0100

    Handle __mips64
    Handle __mips64 as __mips64__ to avoid the following build failure:
    
    makedumpfile.c: In function 'is_kvaddr':
    makedumpfile.c:1613:39: error: 'KVBASE' undeclared (first use in this function)
      return (addr >= (unsigned long long)(KVBASE));
                                           ^~~~~~
    
    Fixes:
     - http://autobuild.buildroot.org/results/94824fa8baa8edb99a5ca245e5561e0c4e430638
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>

--- arch/mips64.c
+++ arch/mips64.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-#ifdef __mips64__
+#if defined(__mips64__) || defined(__mips64)
 
 #include "../print_info.h"
 #include "../elf_info.h"
